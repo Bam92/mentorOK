a4e28aeb221ed9bd4ef49b93583b5f929b274642
Abel Lifaefi Mbula <abelmbula@protonmail.com>
2020-06-10 11:48:11 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+
+import React from 'react'
+
+function App() {
+    
+    return (
+      <div>
+        <h1>Mentor</h1>
+        <div>
+          <img alt="mentor pic" />
+          <div className="mentor-info">
+            <h2>Name here</h2>
+            <h3>Title - Company</h3>
+            <p>Description</p>
+            <p>Tag</p>
+          </div>
+        </div>
+      </div>
+    )
+
+  }
+
+  export default App
+  
