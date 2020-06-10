fbf6c04343b9c886d75a65cad71aa710d7dcf0c2
Abel Lifaefi Mbula <abelmbula@protonmail.com>
2020-06-10 15:39:15 +0200

    style: add more styles to the app

--- src/App.js
+++ src/App.js
@@ -3,10 +3,15 @@ import React from 'react'
 
 import MentorCardList from './mentorCardList'
 
+import './App.css'
+
 function App() {
     
     return (
-      <div>
+      <div style={{
+        width: `80%`,
+        margin: `auto`
+      }}>
         <h1>Consulte nos Mentors </h1>
         <MentorCardList profiles={profiles} />
       </div>
@@ -20,7 +25,7 @@ function App() {
       img: "/images/abel-profile.png",
       title: "Developpeur Full Stack en JS",
       company: "Freelance",
-      description: "lorem",
+      description: "Andrew Coco has been in the online marketing industry since 2012. Past work experience covers many aspects of digital marketing but SEO is the foundation. Having spent time in-house at a start up doing social media, email, and search marketing, at small agencies doing SEO, at global agencies handling SEM (Paid Search), Andrew gained a wealth of knowledge in many aspects of digital marketing channels and how they relate to business goals. Recently, Andrew was brought in as the Ecommerce SEO Manager at GUESS?. Inc. where he was able to ..",
       tag: "js react js nodejs",
       country: "drc",
       availability: "ok"
@@ -30,7 +35,7 @@ function App() {
       img: "/images/abel-profile.png",
       title: "Front-end Dev",
       company: "Kali Academy",
-      description: "lorem",
+      description: "Remarquez bien que cette fonctionnalité ne fait pas partie de React : elle est fournie par des bibliothèques tierces. React n’a pas d’opinion sur la manière dont les styles doivent être définis ; si vous avez un doute, une bonne manière de commencer consiste à définir vos styles dans des fichiers *.css séparés comme d’habitude et à y faire référence en utilisant",
       tag: "Angular Bootstrap",
       country: "drc",
       availability: "ok"
@@ -39,7 +44,7 @@ function App() {
       name: "Ruth BOKOTA",
       img: "/images/abel-profile.png",
       title: "Full stack",
-      company: "xyz",
+      company: "Infoset, Inc.",
       description: "lorem",
       tag: "python sql js django",
       country: "drc",
--- src/mentorCard.js
+++ src/mentorCard.js
@@ -3,21 +3,42 @@ import React from 'react'
 
 const mentorCard = (props) => {
   const profile = props
+  const style = {
+    display: 'flex',
+    border: '2px solid green',
+    marginBottom: `1.5em`,
+    background: '#fff',
+    padding: '1.5em',
+    borderRadius: '.3em'
+  }
 
   return (   
     
-      <div style={{display: `flex`}}>
+      <div style={style}>
         <img alt="mentor pic" src={profile.img}
           style={{
-            width: `5.5em`,
-            height:`5.5em`,
-            borderRadius: `50%`
+            width: `7.5em`,
+            height:`7.5em`,
+            borderRadius: `50%`,
+            margin: `0 3em`
           }}
         />
-        <div className="mentor-info">
+        <div 
+          className="mentor-info"
+          style={{
+            width: `80%`
+          }}
+        >
           <h2> {profile.name} </h2>
           <h3> {profile.title} - {profile.company} </h3>
-          <p>{profile.description}</p>
+          <p 
+            style={{
+              margin: `1.5em 0`,
+              lineHeight: `1.5`
+            }}
+          >
+            {profile.description}
+           </p>
           <p>{profile.tag}</p>
         </div>
        </div>
