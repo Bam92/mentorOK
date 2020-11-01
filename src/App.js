5602fc41fa1333c536cf7f12278a1a6c126311b5
Abel Lifaefi Mbula <abelmbula@protonmail.com>
2020-11-01 13:02:50 +0200

    Downward communication

--- src/App.js
+++ src/App.js
@@ -1,127 +1,53 @@
-import React, { useEffect, useState } from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link,
-  useParams,
-} from "react-router-dom";
+import React from "react";
 
-import Mentors from "./pages/Mentors";
-import MentorCard from "./pages/Mentor";
-import Navigation from "./components/Navigation";
-import Landing from "./pages/Landing";
-import Login from "./pages/Login";
-import Signup from "./pages/Signup";
-import Admin from "./pages/Admin";
-import { AuthContext } from "./context/auth";
-import PrivateRoute from "./PrivateRoute";
-import * as ROUTES from "./constants/routes";
+function App() {
+  const profiles = [
+    {
+      id: 0,
+      name: "Abel Mbula",
+      img: "/images/abel-profile.png",
+      title: "Developpeur Full Stack en JS",
+      company: "Freelance",
+      description:
+        "Andrew Coco has been in the online marketing industry since 2012. Past work experience covers many aspects of digital marketing but SEO is the foundation. Having spent time in-house at a start up doing social media, email, and search marketing, at small agencies doing SEO, at global agencies handling SEM (Paid Search), Andrew gained a wealth of knowledge in many aspects of digital marketing channels and how they relate to business goals. Recently, Andrew was brought in as the Ecommerce SEO Manager at GUESS?. Inc. where he was able to ..",
+      tag: "js react js nodejs",
+    },
+    {
+      id: 1,
+      name: "Sarah Lifaefi",
+      img: "/images/abel-profile.png",
+      title: "Front-end Dev",
+      company: "Kali Academy",
+      description:
+        "Remarquez bien que cette fonctionnalitÃ© ne fait pas partie de React : elle est fournie par des bibliothÃ¨ques tierces. React nâa pas dâopinion sur la maniÃ¨re dont les styles doivent Ãªtre dÃ©finis ; si vous avez un doute, une bonne maniÃ¨re de commencer consiste Ã  dÃ©finir vos styles dans des fichiers *.css sÃ©parÃ©s comme dâhabitude et Ã  y faire rÃ©fÃ©rence en utilisant",
+      tag: "Angular Bootstrap",
+    },
+    {
+      id: 2,
+      name: "Ruth BOKOTA",
+      img: "/images/abel-profile.png",
+      title: "Full stack",
+      company: "Infoset, Inc.",
+      description: "lorem",
+      tag: "python sql js django",
+    },
+  ];
 
-import "./App.css";
-
-function App(props) {
-  const getTokens = JSON.parse(localStorage.getItem("tokens"));
-  const [authTokens, setAuthTokens] = useState(getTokens);
-
-  const setTokens = (data) => {
-    localStorage.getItem("tokens", JSON.stringify(data));
-    setAuthTokens(data);
-  };
-
-  return (
-    <AuthContext.Provider value={{ authTokens, setAuthTokens: setTokens }}>
-      <Router>
-        <div>
-          <Navigation />
-
-          <Switch>
-            <Route path={ROUTES.MENTOR_PROFILE}>
-              <Mentor />
-            </Route>
-            <Route path={ROUTES.MENTORS}>
-              <Mentors profiles={profiles} />
-            </Route>
-            <Route exact path={ROUTES.SIGN_IN} component={Login} />
-            <Route exact path={ROUTES.SIGN_UP} component={Signup} />
-            <PrivateRoute path={ROUTES.ADMIN} component={Admin} />
-            <Route exact path={ROUTES.LANDING} component={Landing} />
-            <Route component={NotFoundPage} />
-          </Switch>
-        </div>
-      </Router>
-    </AuthContext.Provider>
-  );
+  return <Mentors list={profiles} />;
 }
 
-const NotFoundPage = () => {
-  return (
-    <div>
-      <h3>404 - Not found</h3>
-      <p>
-        You should need to return <Link to="/">home</Link>
-      </p>
+const Mentors = props =>
+  props.list.map(mentor => (
+    <div key={mentor.id}>
+      <h1>
+        {mentor.id}. {mentor.name}
+      </h1>
+      <ul>
+        <li>Title: {mentor.title}</li>
+        <li>Company: {mentor.company}</li>
+        <li>Description: {mentor.description}</li>
+      </ul>
     </div>
-  );
-};
-
-export function Mentor() {
-  const { mentorname } = useParams();
-  let profile = profiles.find((mentor) => mentor.name === mentorname);
-
-  useEffect(() => {
-    document.title = `MentorOK - Profile de ${mentorname}`;
-  });
-
-  return (
-    <div
-      style={{
-        width: `80%`,
-        margin: `auto`,
-      }}
-    >
-      <h1>Profile de {profile.name} </h1>
-      <MentorCard {...profile} />
-    </div>
-  );
-}
-
-export const profiles = [
-  {
-    id: 0,
-    name: "Abel Mbula",
-    img: "/images/abel-profile.png",
-    title: "Developpeur Full Stack en JS",
-    company: "Freelance",
-    description:
-      "Andrew Coco has been in the online marketing industry since 2012. Past work experience covers many aspects of digital marketing but SEO is the foundation. Having spent time in-house at a start up doing social media, email, and search marketing, at small agencies doing SEO, at global agencies handling SEM (Paid Search), Andrew gained a wealth of knowledge in many aspects of digital marketing channels and how they relate to business goals. Recently, Andrew was brought in as the Ecommerce SEO Manager at GUESS?. Inc. where he was able to ..",
-    tag: "js react js nodejs",
-    country: "drc",
-    availability: "ok",
-  },
-  {
-    id: 1,
-    name: "Sarah Lifaefi",
-    img: "/images/abel-profile.png",
-    title: "Front-end Dev",
-    company: "Kali Academy",
-    description:
-      "Remarquez bien que cette fonctionnalité ne fait pas partie de React : elle est fournie par des bibliothèques tierces. React n’a pas d’opinion sur la manière dont les styles doivent être définis ; si vous avez un doute, une bonne manière de commencer consiste à définir vos styles dans des fichiers *.css séparés comme d’habitude et à y faire référence en utilisant",
-    tag: "Angular Bootstrap",
-    country: "drc",
-    availability: "ok",
-  },
-  {
-    id: 2,
-    name: "Ruth BOKOTA",
-    img: "/images/abel-profile.png",
-    title: "Full stack",
-    company: "Infoset, Inc.",
-    description: "lorem",
-    tag: "python sql js django",
-    country: "drc",
-    availability: "ok",
-  },
-];
+  ));
 
 export default App;
--- src/constants/routes.js
+++
@@ -1,6 +0,0 @@
-export const LANDING = "/";
-export const SIGN_UP = "/auth/signup";
-export const SIGN_IN = "/auth/login";
-export const MENTORS = "/mentors";
-export const ADMIN = "/admin";
-export const MENTOR_PROFILE = "/mentors/:mentorname"
